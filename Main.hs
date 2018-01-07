209ed837f784d1ea12d4cb5d55a58b1693c58c1a
evertonportela <evertonportela@outlook.com>
2018-01-07 16:42:24 -0300

    Update Main.hs

--- Main.hs
+++ Main.hs
@@ -1,9 +1,14 @@
 module Main(main) where
 
 import Graphics.Gloss
+    
+width, height, offset :: Int
+width = 300
+height = 300
+offset = 100
 
 window :: Display
-window = InWindow "Nice Window" (200, 200) (10, 10)
+window = InWindow "LPSI - Atv9 Game Haskell Pong" (200, 200) (20, 20)
 
 background :: Color
 background = white
