2295655df2979446d3f41f6b25e627de23d701b1
evertonportela <evertonportela@outlook.com>
2018-01-07 17:07:05 -0300

    Update Main.hs

--- Main.hs
+++ Main.hs
@@ -8,13 +8,41 @@ height = 300
 offset = 100
 
 window :: Display
-window = InWindow "LPSI - Atv9 Game Haskell Pong" (200, 200) (20, 20)
+window = InWindow "LPSI - Atv9 Game Haskell Pong" (width, height) (offset, offset)
 
 background :: Color
-background = white
+background = black
 
 drawing :: Picture
-drawing = circle 80
+drawing = pictures [ ball, 
+                     walls, 
+                     mkPaddle rose 120 (-20), 
+                     mkPaddle orange (-120) 40]
+          where
+            -- The pong balll - A bola do Pong
+            ball = translate (-10) 40 $ color ballColor $ circleSolid 10
+            ballColor = dark red
+
+            -- The bottom and top walls - As paredes Inferiores e Superiores
+            wall :: Float -> Picture
+            wall offset = translate 0 offset $ color wallColor $ rectangleSolid 270 10
+
+            wallColor = greyN 0.5
+            walls = pictures [wall 150, wall (-150)]
+            
+            --  Make a paddle of a given border and vertical offset.
+            --  Faça uma paleta de uma determinada borda e deslocamento vertical
+            mkPaddle :: Color -> Float -> Float -> Picture
+            mkPaddle col x y = pictures 
+              [ color col $ rectangleSolid 26 86]
+            {- drawing :: Picture
+drawing = pictures
+        [translate (-20) (-100) $ color ballColor $ circleSolid 30,
+        translate 30 50 $ color paddleColor $ rectangleSolid 10 50
+        ]
+        where
+          ballColor = dark red
+          paddleColor = light (light blue) -}
 
 main :: IO ()
 main = display window background drawing
